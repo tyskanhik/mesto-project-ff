4c1b731709a215e41f37198bb41986c610f8b18d
Arctic Fox <tyskanhik1@mail.ru>
2024-01-17 13:32:40 +0300

    реализация вывода карточек на сайт

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
+// копируем 
+
+let cards = initialCards.slice();
+
+
+
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupTypeImage = document.querySelector('.popup_type_image');
 
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+cards.forEach((card) => {
+
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
+    console.log(cardElement);
+
+    cardElement.querySelector('.card__title').textContent = card.name;
+    
+    placesList.append(cardElement);
+})
