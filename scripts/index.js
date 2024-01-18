801130ff2be6f25de23631f9e47da01e29e529d1
Arctic Fox <tyskanhik1@mail.ru>
2024-01-18 21:43:26 +0300

    контрольные правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,44 @@
-// копируем 
+// копируем входящий массив с карточками
 
 let cards = initialCards.slice();
+
 
 // @todo: Темплейт карточки
 
 const cardTemplate = document.querySelector('#card-template').content;
 
+
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
+
+
+// модальные окна
+
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 const popupTypeImage = document.querySelector('.popup_type_image');
 
-// @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
 function delitedCard(element) {
     const deleteButton = element.querySelectorAll('.card__delete-button');
-    // console.log(...deleteButton);
     deleteButton.forEach((button) => {
-        button.addEventListener('click', (e) => {
+        button.addEventListener('click', () => {
             button.parentElement.remove();
         })
     })
 }
 
+
+// @todo: Функция создания карточки
 // @todo: Вывести карточки на страницу
 
 cards.forEach((card) => {
-
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__image').alt = card.name;
-    // console.log(cardElement);
-
     cardElement.querySelector('.card__title').textContent = card.name;
     
     placesList.append(cardElement);
